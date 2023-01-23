e0f61e6f2583bc9d334676753a33fcf02f9a2824
RandomPro8 <59402575+RandomPro8@users.noreply.github.com>
2023-01-23 07:11:00 -0800

    Framework for auto-allignment, ascii art header

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,12 +2,29 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/*
+__________         __         ._____.           __          
+\______   \_____ _/  |________|__\_ |__   _____/  |_  ______
+ |     ___/\__  \\   __\_  __ \  || __ \ /  _ \   __\/  ___/
+ |    |     / __ \|  |  |  | \/  || \_\ (  <_> )  |  \___ \ 
+ |____|    (____  /__|  |__|  |__||___  /\____/|__| /____  >
+                \/                    \/                 \/ 
+           ______________________    ______                 
+          /  |  \______  \_____  \  /  __  \                
+         /   |  |_  /    / _(__  <  >      <                
+        /    ^   / /    / /       \/   --   \               
+        \____   | /____/ /______  /\______  /               
+             |__|               \/        \/                                                        
+*/                                         
+
+
 package frc.robot;
 
 import java.util.List;
 
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
+import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 import org.photonvision.targeting.TargetCorner;
 
@@ -24,6 +41,7 @@ import edu.wpi.first.networktables.Topic;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -46,6 +64,10 @@ public class Robot extends TimedRobot {
 
   private static final double CAMERA_HEIGHT_METERS = 0;
 
+  private static final double rotDeadzone = 0.1;
+
+  private static final double xDeadZone = 0.1;
+
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
@@ -59,11 +81,6 @@ public class Robot extends TimedRobot {
   
   JSON json = new JSON("data.json");
 
-  // SmartDashboard dashboard = new SmartDashboard();
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
   @Override
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
@@ -71,13 +88,6 @@ public class Robot extends TimedRobot {
     m_robotContainer = new RobotContainer();
   }
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
   @Override
   public void robotPeriodic() {
     // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
@@ -87,14 +97,12 @@ public class Robot extends TimedRobot {
     CommandScheduler.getInstance().run();
   }
 
-  /** This function is called once each time the robot enters Disabled mode. */
   @Override
   public void disabledInit() {}
 
   @Override
   public void disabledPeriodic() {}
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
@@ -105,34 +113,153 @@ public class Robot extends TimedRobot {
     }
   }
 
-  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {}
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-  }
 
-  /** This function is called periodically during operator control. */
+  }
+  
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+
+    // Use the A button to acrivate the allignment process
+    if (operator.getAButton()){
+
+      // Get the latest data from photonvision
+      PhotonPipelineResult result = camera.getLatestResult();
+
+      // Make sure that the camera has tags in view
+      if (result.hasTargets()){
+
+        PhotonTrackedTarget target = result.getBestTarget();
+
+        double yaw = target.getYaw();
+        double pitch = target.getPitch();
+        Transform3d pose = target.getBestCameraToTarget();
+        int tagID = target.getFiducialId();
+        
+        // Direction is Left: -1, Right: 1, Default: 0
+        int direction = 0;
+
+        if (yaw > rotDeadzone){
+          // Rotate the robot in the negative yaw direction
+        }
+        
+        else if (yaw < -rotDeadzone){
+          // Rotate the robot in the positive yaw direction
+        }
+
+        /**
+         * The operator has two buttons they can press, 
+         * one to move to the position to the left of the tag or to the right.
+         * 
+         * The distances for these positions are set as an array tagPositions,
+         * which follows format:
+         * {1:[leftDist, rightDist], 2:[leftDist, rightDist], ...}
+         */
+
+        /**
+         *  A visual representation of the apriltag positions
+         *  / --------------------------------------------- \ 
+         *  5                      |                        4
+         *  |                      |                        |
+         *  |                      |                        |
+         *  6                      |                        3
+         *  |                      |                        |
+         *  7                      |                        2
+         *  |                      |                        |
+         *  8                      |                        1
+         *  \ --------------------------------------------- /
+         */
+
+        else {
+
+          // Get the direction the operator wants to move
+          if (operator.getLeftBumper()){
+            direction = -1;
+          }
+          else if (operator.getRightBumper()){
+            direction = 1;
+          }
+          
+            // There are different distances for each tag
+            switch(tagID){
+              case 1:
+                if (direction == 1){
+                  // Move the robot right x amount
+                }
+                else if (direction == -1){
+                  // Move the robot left x amount
+                }
+                break;
+
+              case 2:
+                if (direction == 1){
+                  // Move the robot right x amount
+                }
+                else if (direction == -1){
+                  // Move the robot left x amount
+                }
+                break;
+              
+              case 3:
+                if (direction == 1){
+                  // Move the robot right x amount
+                }
+                else if (direction == -1){
+                  // Move the robot left x amount
+                }
+                break;
+              
+              case 6:
+                if (direction == 1){
+                  // Move the robot right x amount
+                }
+                else if (direction == -1){
+                  // Move the robot left x amount
+                }
+                break;
+
+              case 7:
+                if (direction == 1){
+                  // Move the robot right x amount
+                }
+                else if (direction == -1){
+                  // Move the robot left x amount
+                }
+                break;
+
+              case 8:
+                if (direction == 1){
+                  // Move the robot right x amount
+                }
+                else if (direction == -1){
+                  // Move the robot left x amount
+                }
+                break;
+              
+              // Makes the controller buzz if pointed at an invalid tag
+              default:
+                operator.setRumble(RumbleType.kBothRumble, 0.5);
+
+            }
+          }
+        }
+      }
+
+      // Make the controller buzz if there are no targets in view
+      else {
+        operator.setRumble(RumbleType.kBothRumble, 0.5);
+      }
+
+    }
 
 
 
   
-  //   PhotonTrackedTarget target = camera.getLatestResult().getBestTarget();
-  // double yaw = target.getYaw();
-  // double pitch = target.getPitch();
-  // double area = target.getArea();
-  // double skew = target.getSkew();
-  // Transform2d pose = target.getCameraToTarget()
+  
 
 
   @Override
